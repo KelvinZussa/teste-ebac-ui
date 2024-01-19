b79222212174020ba1275c4ecfa8e7a3eab632eb
Kelvin Zussa <kelvinzussa@hotmail.com>
2024-01-18 22:38:08 -0300

    alterações no teste automatizado

---
+++ cypress/e2e/produtos.spec.cy.js
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+
+describe('Página de Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+
+    });
+
+    it('Deve selecinar um produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+            //.first()
+            //.last()
+            //.eq(3)
+            .contains('Ariel Roll Sleeve Sweatshirt')
+            .click()
+
+    });
+
+    it('Deve selecionar ao carrinho', () => {
+       var quantidade = 3
+
+        cy.get('[class="product-block grid"]')
+    .contains('Ariel Roll Sleeve Sweatshirt').click()
+    cy.get('.button-variable-item-XS').click()
+    cy.get('.button-variable-item-Purple').click()
+    cy.get('.input-text').clear().type(quantidade)
+    cy.get('.single_add_to_cart_button').click()
+
+    cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+    cy.get('.woocommerce-message').should('contain', quantidade + ' × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.')
+        
+    });
+    
+
+});
+
+
+
+
